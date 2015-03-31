c98678b2400e0fb76f1562686bdff3db86021f39
ktravers <travers.kt@gmail.com>
2015-03-31 18:38:16 -0400

    update user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :games, :foreign_key => 'host_id'
 	has_many :reservations, through: :games
-  has_many :events, :foreign_key => 'player_id', :class_name => "Reservation"  
+  has_many :reservations, :foreign_key => 'player_id'
 end
