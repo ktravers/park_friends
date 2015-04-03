c101b17dedadcb40bbcc6440d2fdc9f04fa042cf
ktravers <travers.kt@gmail.com>
2015-04-02 22:13:40 -0400

    start signup form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,15 @@
 class ReservationsController < ApplicationController
 
   def new 
+    @reservation = Reservation.new
   end
 
+  # good spot to ajax?
   def create
+    @reservation = Reservation.new(game_params)
+    if @reservation.save
+      # redirect_to @reservation, :notice => "Congrats! You're playing a game!"
+    end
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,17 +4,20 @@ class Game < ActiveRecord::Base
   has_many :reservations
   has_many :players, :class_name => "User", through: :reservations
 
-  # boolean check
+  # boolean check - does this game have any reservations?
   def has_reservations?
     self.reservations.length > 0
   end
 
+  # boolean check - does this game have any open spots?
+  def has_open_spots?
+    open_spots > 0
+  end
+
+  # returns number of open spots
   def open_spots
     if has_reservations?
-      self.player_limit - self.reservations.length
-      if (self.player_limit - self.reservations.length) == 0
-        "Sorry! This game is full."
-      end
+      self.player_limit - self.reservations.length  
     else
       self.player_limit
     end
